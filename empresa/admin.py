1f087952ee02ae7d3c5d258973b18ce0497ae63b
Davi-Ga <davigaldinoky@gmail.com>
2022-11-08 12:35:30 -0300

    admins adicionado

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tecnologia,Empresa,Vaga
 
-# Register your models here.
+admin.site.register(Tecnologia)
+admin.site.register(Empresa)
+admin.site.register(Vaga)
+
