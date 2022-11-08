aad8458fbbb6a98a5fc7405ea59cc6680dbb30d9
Davi-Ga <davigaldinoky@gmail.com>
2022-11-08 12:35:45 -0300

    vagas e nova lista de choices

--- empresa/models.py
+++ empresa/models.py
@@ -7,6 +7,20 @@ CHOICES_NICHO=(
     ('T','Tecnologia'),
     ('P','Psicologia')
 )
+CHOICES_EXPERIENCIA = (
+    ('J', 'Júnior'),
+    ('P', 'Pleno'),
+    ('S', 'Sênior')
+)
+
+CHOICES_STATUS = (
+    ('I', 'Interesse'),
+    ('C', 'Currículo enviado'),
+    ('E', 'Entrevista'),
+    ('D', 'Desafio técnico'),
+    ('F', 'Finalizado')
+)
+
 class Tecnologia(models.Model):
     tecnologia=models.CharField(max_length=30,null=False,blank=False)
     
@@ -25,5 +39,17 @@ class Empresa(models.Model):
     
     def __str__(self):
         return self.nome
+
+class Vaga(models.Model):
     
+    empresa = models.ForeignKey(Empresa, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=30)
+    nivel_experiencia = models.CharField(max_length=2, choices=CHOICES_EXPERIENCIA)
+    data_final = models.DateField()
+    status = models.CharField(max_length=30, choices=CHOICES_STATUS)
+    tecnologias_dominadas = models.ManyToManyField(Tecnologia)
+    tecnologias_estudar = models.ManyToManyField(Tecnologia, related_name='estudar')
+
+    def __str__(self):
+        return self.titulo
     
