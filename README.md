6d072ef88c461fddffe5eecb629f5d50ad3eeeb2
Davi-Ga <davigaldinoky@gmail.com>
2022-11-09 23:29:54 -0300

    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# selet-ie
-Projeto para gerenciar processos seletivos nas quais usuário participa
--- empresa/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tecnologia,Empresa,Vaga
-
-admin.site.register(Tecnologia)
-admin.site.register(Empresa)
-admin.site.register(Vaga)
-
