3e642e4c670092f2e396c37506b3d2d714e3995d
Davi-Ga <davigaldinoky@gmail.com>
2022-11-09 23:30:03 -0300

    a

---
+++ empresa/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Tecnologia,Empresa,Vaga
+
+admin.site.register(Tecnologia)
+admin.site.register(Empresa)
+admin.site.register(Vaga)
+
---
+++ empresa/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Tecnologia,Empresa
+from django.contrib import messages
+from django.contrib.messages import constants
+from . import models
+
+def nova_empresa(request):
+    if request.method=='GET':
+        tecs=Tecnologia.objects.all()
+    elif request.method=='POST':
+        nome=request.POST.get('nome')
+        email=request.POST.get('email')
+        cidade=request.POST.get('cidade')
+        endereco=request.POST.get('endereco')
+        nicho=request.POST.get('nicho')
+        caracteristicas=request.POST.get('caractetisticas')
+        tecnologias=request.POST.getlist('tecnologias')
+        logo=request.FILES.get('logo')
+
+        if logo.size > 100_000_000:
+            messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB')
+            return redirect('/nova_empresa')
+
+        if nicho not in [i[0] for i in models.CHOICES_NICHO]:
+            messages.add_message(request, constants.ERROR, 'Nicho de mercado inválido')
+            return redirect('/nova_empresa')
+        
+        empresa = Empresa(
+            logo=logo,
+            nome=nome,
+            email=email,
+            cidade=cidade,
+            endereco=endereco,
+            nicho=nicho,
+            caracteristicas=caracteristicas
+        )
+        empresa.save()
+        empresa.tecnologias.add(*tecnologias)
+        empresa.save()
+        messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
+        
+        return redirect('/empresas')
+    context={
+            'tecs':tecs,
+
+        }
+    return render(request,'nova_empresa.html',context=context)
+
+def empresas(request):
+    empresas=Empresa.objects.all()
+    tecnologias=Tecnologia.objects.all()
+    context={
+        'empresas':empresas,
+        'tecnologias':tecnologias
+    }
+    
+    return render(request,'empresas.html',context=context)
+
+def excluir_empresa(request, id):
+    empresa = Empresa.objects.get(id=id)
+    empresa.delete()
+    messages.add_message(request, constants.SUCCESS, 'Empresa excluída com sucesso')
+    
+    return redirect('/empresas')
